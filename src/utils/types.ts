a9dc2d1fb0f247dfc7976948a5737730e02504c6
Dang Gia Loc <hahagioi169@gmail.com>
2024-06-30 22:44:30 +0700

    add utils constants, helpers, types . ts

---
+++ src/utils/types.ts
@@ -0,0 +1,13 @@
+export type Algorithm = "DIJKSTRA" | "A_STAR" | "BFS" | "DFS";
+export type MazeType = "NONE" | "BINARY_TREE" | "RECURSIVE_DIVISION";
+export type TileType = {
+    row: number;
+    col: number;
+    isEnd: boolean;
+    isWall: boolean;
+    isPath: boolean;
+    distance: number;
+    isStart: boolean;
+    parent: TileType | null;
+};
+export type GridType = TileType[][];
