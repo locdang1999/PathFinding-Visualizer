c84915ea505d577c6e839312313e68ca3ef04b55
Dang Gia Loc <hahagioi169@gmail.com>
2024-07-09 21:58:53 +0700

    create Tile Context

---
+++ src/context/TileContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, ReactNode, useState } from "react";
+import { TileType } from "../utils/types";
+import { END_TILE_CONFIGURATION, START_TILE_CONFIGURATION } from "../utils/constants";
+
+interface TitleContextInterface {
+    startTile: TileType;
+    setStartTile: (startTile: TileType) => void;
+    endTile: TileType;
+    setEndTile: (endTile: TileType) => void;
+}
+
+export const TileContext = createContext<TitleContextInterface | undefined>(undefined);
+
+export const TileProvider = ({ children }: { children: ReactNode }) => {
+    const [startTile, setStartTile] = useState<TileType>(START_TILE_CONFIGURATION);
+    const [endTile, setEndTile] = useState<TileType>(END_TILE_CONFIGURATION);
+
+    return (
+        <TileContext.Provider value={{
+            startTile,
+            setStartTile,
+            endTile,
+            setEndTile
+        }}>
+            {children}
+        </TileContext.Provider>
+    )
+}
